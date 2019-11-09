06a935f5fcbc5b305dc1d31c816a59931843032e
hugonguyen <nguyenquoctuan61089@gmail.com>
2019-11-09 18:16:53 +0700

    passing data using children

--- src/mern-stack/public/App.js
+++ src/mern-stack/public/App.js
@@ -88,7 +88,7 @@ function (_React$Component3) {
         style: style
       }, this.props.issue_id), React.createElement("td", {
         style: style
-      }, this.props.issue_title));
+      }, this.props.children));
     }
   }]);
 
@@ -122,13 +122,11 @@ function (_React$Component4) {
         style: rowStyle
       }, "Title"))), React.createElement("tbody", null, React.createElement(IssueRow, {
         rowStyle: rowStyle,
-        issue_id: 1,
-        issue_title: "Error in console when clicking Add"
-      }), React.createElement(IssueRow, {
+        issue_id: 1
+      }, "Error in console when clicking Add"), React.createElement(IssueRow, {
         rowStyle: rowStyle,
-        issue_id: 2,
-        issue_title: "Missing bottom border on panel"
-      })));
+        issue_id: 2
+      }, React.createElement("div", null, "Missing ", React.createElement("b", null, "bottom"), " border on panel"))));
     }
   }]);
 
