452bc22662ddc26b6e47803e8333f1db76d82b36
hugonguyen <nguyenquoctuan61089@gmail.com>
2019-11-13 22:36:39 +0700

    List API Integration

--- src/mern-stack/src/App.jsx
+++ src/mern-stack/src/App.jsx
@@ -1,25 +1,27 @@
 
-//For explicitness, rename 'issues' -> 'initialIssues'
-const initialIssues = [
-    {
-        id: 1,
-        status: 'New',
-        owner:'Ravan',
-        effort: 5,
-        created: new Date('2018-08-15'),
-        due: undefined,
-        title: 'Error in console when clicking Add'
-    },
-    {
-        id: 2,
-        status: 'Assigned',
-        owner:'Eddie',
-        effort: 14,
-        created: new Date('2018-08-16'),
-        due: new Date('2018-08-30'),
-        title: 'Missing bottom border on panel'
-    }
-]
+
+// const initialIssues = [
+//     {
+//         id: 1,
+//         status: 'New',
+//         owner:'Ravan',
+//         effort: 5,
+//         created: new Date('2018-08-15'),
+//         due: undefined,
+//         title: 'Error in console when clicking Add'
+//     },
+//     {
+//         id: 2,
+//         status: 'Assigned',
+//         owner:'Eddie',
+//         effort: 14,
+//         created: new Date('2018-08-16'),
+//         due: new Date('2018-08-30'),
+//         title: 'Missing bottom border on panel'
+//     }
+// ]
+
+
 
 // const sampleIssue = {
 //     status: 'New', owner: 'Pieta', title: 'Completion date should be optional'
@@ -33,23 +35,6 @@ class IssueFilter extends React.Component {
     }
 }
 
-// class IssueRow extends React.Component {
-//     render(){
-//         const issue = this.props.issue;
-//         return(
-//             <tr>
-//                 <td>{issue.id}</td>
-//                 <td>{issue.status}</td>
-//                 <td>{issue.owner}</td>
-//                 <td>{issue.created.toDateString()}</td>
-//                 <td>{issue.effort}</td>
-//                 <td>{issue.due? issue.due.toDateString():''}</td>
-//                 <td>{issue.title}</td>
-//             </tr>
-//         );
-//     }
-// }
-
 function IssueRow(props){
     const issue = props.issue;
     return(
@@ -57,44 +42,13 @@ function IssueRow(props){
             <td>{issue.id}</td>
             <td>{issue.status}</td>
             <td>{issue.owner}</td>
-            <td>{issue.created.toDateString()}</td>
+            <td>{issue.created}</td>
             <td>{issue.effort}</td>
-            <td>{issue.due? issue.due.toDateString():''}</td>
+            <td>{issue.due? issue.due:''}</td>
             <td>{issue.title}</td>
         </tr>
     );
 }
-
-// class IssueTable extends React.Component {
-
-//     constructor(){
-//         super();
-//     }
-
-//     render(){
-
-//         const issueRows = this.props.issues.map(issue=><IssueRow key={issue.id} issue={issue}/>);
-        
-//         return(
-//             <table className="bordered-table">
-//                 <thead>
-//                     <tr>
-//                         <th>ID</th>
-//                         <th>Status</th>
-//                         <th>Owner</th>
-//                         <th>Created</th>
-//                         <th>Effort</th>
-//                         <th>Due Date</th>
-//                         <th>Title</th>
-//                     </tr>
-//                 </thead>
-//                 <tbody>
-//                     {issueRows}
-//                 </tbody>
-//             </table>
-//         );
-//     }
-// }
 
 function IssueTable(props){
     const issueRows = props.issues.map(issue=><IssueRow key={issue.id} issue={issue}/>);
@@ -165,12 +119,27 @@ class IssueList extends React.Component {
         this.loadData();
     }
 
-    loadData(){
-        setTimeout(()=>{
-            this.setState({
-                issues:initialIssues
-            });
-        },500);
+    async loadData(){
+        // setTimeout(()=>{
+        //     this.setState({
+        //         issues:initialIssues
+        //     });
+        // },500);
+        //Change memory variable initialIssues to fetch from api
+        const query = `query {
+            issueList{
+                id title status owner created effor due
+            }
+        }`;
+
+        const response = await fetch('/graphql',{
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({query})
+        })
+
+        const result = await response.json();
+        this.setState({issues:result.data.issueList});
     }
 
     createIssue(issue){
