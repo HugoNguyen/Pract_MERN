614918b97ca562f6eeb13e57562ed0b1737d0dc6
HugoNguyen <nguyenquoctuan61089@gmail.com>
2019-12-01 17:28:24 +0700

    commit chap 06

---
+++ src/mern-stack/scripts/trymongo.js
@@ -0,0 +1,9 @@
+const {MongoClient} = require('mongodb');
+
+//For localhost
+//const url = 'mongodb://localhost/issuetracker';
+
+//Atlas URL
+const url = 'mongodb+srv://hugo:123QWE@cluster0-brgc0.azure.mongodb.net/issuetracker?retryWrites=true';
+
+
